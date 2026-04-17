7bc42c1f57826b2bbbc15f21142036a65e8a6000
Stephan Hohe <sth.dev@tejp.de>
2026-04-17 15:41:46 +0200

    Show commands for publishing and tagging

--- build.sh
+++ build.sh
@@ -23,6 +23,11 @@ fi
 echo "Preparing package version $VERSION"
 deno run -A build.ts "$VERSION"
 
-echo 'The package is prepared in `npm/` and can be published there with `npm publish`'
+echo
+echo "The package version $VERSION is prepared in npm/ and can be published with:"
+echo '    ( cd npm; npm publish )'
+echo
+echo 'Tag the release in git:'
+echo "    git -C xmlp tag 'npm-v$VERSION'"
 
 echo "$VERSION" > .version
