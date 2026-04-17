212cc122883f22e12866e7aea6651e334c2298d9
Stephan Hohe <sth.dev@tejp.de>
2026-04-17 15:32:53 +0200

    Add version management

--- build.sh
+++ build.sh
@@ -3,7 +3,26 @@ set -eu
 
 cd "$(dirname "$0")"
 
-echo "Preparing package"
-deno run -A build.ts "$@"
+
+if [[ -v 1 ]]; then
+	VERSION="$1"
+else
+	VERSION="0.0.0"
+	if [[ -e .version ]]; then
+		read VERSION < .version
+	fi
+
+	echo -n "Package version (default $VERSION): "
+	read NVERSION
+
+	if [[ -n "$NVERSION" ]]; then
+		VERSION="$NVERSION"
+	fi
+fi
+
+echo "Preparing package version $VERSION"
+deno run -A build.ts "$VERSION"
 
 echo 'The package is prepared in `npm/` and can be published there with `npm publish`'
+
+echo "$VERSION" > .version
